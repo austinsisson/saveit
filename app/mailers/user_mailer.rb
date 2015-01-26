cf98b6d8ce0ed51473014fd9c3ed4a4130c57cd2
Austin Sisson <austinsisson@gmail.com>
2015-01-26 03:21:28 +0000

    fixed typo in usermailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer::Base
   def bookmark_creation_failure(params)
-    mail(to: params[:sender], subject: 'Bookmark Creation Failed!', from: 'admin@saveit.com'
+    mail(to: params[:sender], subject: 'Bookmark Creation Failed!', from: 'admin@saveit.com')
   end
 end
