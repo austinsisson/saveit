c11546a24ff9fdb4ad5abd13595a464816a240b9
Austin Sisson <austinsisson@gmail.com>
2015-01-26 03:13:27 +0000

    changed error mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,5 @@
+class UserMailer < ApplicationMailer::Base
+  def bookmark_creation_failure(params)
+    mail(to: params[:sender], subject: 'Bookmark Creation Failed!', from: 'admin@saveit.com'
+  end
+end
--- app/services/create_bookmark.rb
+++ app/services/create_bookmark.rb
@@ -17,15 +17,7 @@ class CreateBookmark
       bookmark.save
     rescue LinkThumbnailer::BadUriFormat
       bookmark.destroy
-      create_failure
+      UserMailer.bookmark_creation_failure(params)
     end
-  end
-  
-  def create_failure(params)
-    RestClient.post 'https://api:"#{MAILGUN_API_KEY}"@api.mailgun.net/V2/app32619189.mailgun.org/messages',
-    from:     "Admin <admin@saveit.com>",
-    to:       params[:sender],
-    subject:  "Create Bookmark Failed!",
-    text:     "Oops! Looks like there was an issue with the bookmark you sent us. Please try again. -SaveIt Admin Team"
   end
 end
