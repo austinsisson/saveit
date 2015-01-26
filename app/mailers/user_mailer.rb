129fe65771c7fe38d7444e02040c9ffaf63321d1
Austin Sisson <austinsisson@gmail.com>
2015-01-26 03:23:50 +0000

    ...and again.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,4 @@
-class UserMailer < ApplicationMailer::Base
+class UserMailer < ActionMailer::Base
   def bookmark_creation_failure(params)
     mail(to: params[:sender], subject: 'Bookmark Creation Failed!', from: 'admin@saveit.com')
   end
